c052a5a3ba61d6905106d903409d0dffe40e6eb2
Zoltan Zsigmond Kiss <kiss.zoltan.pti@gmail.com>
2018-09-20 22:48:48 +0200

    Version: 0.1.17 [All Page] - Follow the New Design on every Site. #21 | Part 1/2 [All Page] - Re Design the FOOTER #5 | Part 1/2 [All Page] - Re Design the TOP MENU #6 | Part 1/2

--- src/main/java/com/myFinance/config/SecurityConfig.java
+++ src/main/java/com/myFinance/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/home","/transaction","/summary","/transaction/make").permitAll()
+                .antMatchers("/","/home","/about","/transaction","/summary","/transaction/make").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("USER")
                 .anyRequest().authenticated()
