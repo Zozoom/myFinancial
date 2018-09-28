8315a0d551e643e20196767de29b5e6d7498baf0
Zozoom <kiss.zoltan.pti@gmail.com>
2018-09-28 22:59:17 +0200

    [Tech] - Create properties Profiels for each environment. #23

--- src/main/java/com/myFinance/MainFinanceApplication.java
+++ src/main/java/com/myFinance/MainFinanceApplication.java
@@ -2,6 +2,7 @@ package com.myFinance;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,42 +10,63 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class MainFinanceApplication {
 
     private static final Logger log = LoggerFactory.getLogger(MainFinanceApplication.class);
 
-    private static String myUrl = "http://localhost:80/";
+    /**
+     * Get values from properties file.
+     * */
+    private static String profile;
+    private static String serverPort;
 
-    public static void main(String[] args) {
+    @Value("${spring.profiles.active}")
+    public void setProfile(String profile) {
+        MainFinanceApplication.profile = profile.replace("\"","").toUpperCase();
+    }
+
+    @Value("${server.port}")
+    public void setServerPort(String serverPort) {
+        MainFinanceApplication.serverPort = serverPort.replace("\"","").toUpperCase();
+    }
+
+    /**
+     * The Main method itself.
+     * */
+    public static void main(String[] args){
         SpringApplication.run(MainFinanceApplication.class, args);
 
-        System.out.println("\n******************* Fapp Application Enviroment Variables *************************\n");
-        System.out.println("qa_master_user:     [" + System.getenv("qa_master_user") + "]");
-        System.out.println("qa_master_password: [" + System.getenv("qa_master_password") + "]");
-        System.out.println("qa_email_address:   [" + System.getenv("qa_email_address") + "]");
-        System.out.println("qa_email_password:  [" + System.getenv("qa_email_password") + "]");
-        System.out.println("\n***********************************************************************************\n");
-
         log.info("||////////////////////////////////////////////////////////////////////////////////////////||");
-        log.info("||                             Financial Application                                      ||");
+        log.info("||                                 Family Budget                                          ||");
         log.info("||                             Created by Zoltan Kiss                                     ||");
         log.info("||                                 Copyright 2018                                         ||");
         log.info("||////////////////////////////////////////////////////////////////////////////////////////||");
+        log.info("******************* Family Budget Application Environment Variables *************************");
+        log.info("Active Profile:           [" + profile + "]");
+        log.info("Server Port:              [" + serverPort + "]");
+        log.info(profile+"_DB_URL:          [" + System.getenv(profile+"_DB_URL") + "]");
+        log.info(profile+"_DB_USER:         [" + System.getenv(profile+"_DB_USER") + "]");
+        log.info(profile+"_DB_PASSWORD:     [" + System.getenv(profile+"_DB_PASSWORD") + "]");
+        log.info(profile+"_EMAIL_ADDRESS:   [" + System.getenv(profile+"_EMAIL_ADDRESS") + "]");
+        log.info(profile+"_EMAIL_PASSWORD:  [" + System.getenv(profile+"_EMAIL_PASSWORD") + "]");
+        log.info("***********************************************************************************");
         log.info(">> [FinancApplication.main] - Starting Application ... Please Stand by...");
 
-
         try {
+            String myUrl = "http://localhost:"+serverPort;
             openBrowser(myUrl);
         }
         catch (Exception e){
-            System.out.println("\nThere was an error: "+e+"\n");
+            log.info("\nThere was an error: "+e+"\n");
         }
 
     }
 
+    /**
+     *
+     * */
     private static void openBrowser(String url) {
         log.info(">> [openBrowser.main] - Automatically open the app in a browser...");
 
@@ -52,7 +74,7 @@ public class MainFinanceApplication {
             Desktop desktop = Desktop.getDesktop();
             try {
                 desktop.browse(new URI(url));
-                log.info(">> [openBrowser.main] - Browser opened at: {}", myUrl);
+                log.info(">> [openBrowser.main] - Browser opened at: {}", url);
             } catch (IOException | URISyntaxException e) {
                 e.printStackTrace();
             }
@@ -65,5 +87,5 @@ public class MainFinanceApplication {
             }
         }
     }
-
 }
+
